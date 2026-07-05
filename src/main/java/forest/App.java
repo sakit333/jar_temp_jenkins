77d30508b508150a7bc4919c74a21b92a37b4c9b
sakit333 <sak528264@gmail.com>
2026-07-05 13:35:11 +0530

    done

--- src/main/java/forest/App.java
+++ src/main/java/forest/App.java
@@ -1,10 +1,10 @@
 package forest;
 
 /**
- * Hello world!
+ * Hello DevOps!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello DevOps!");
     }
 }
